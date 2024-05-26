f7b0b1b6798abadacf588229f36ee368482df6f5
Hankrezu <hankrezu@gmail.com>
2024-05-26 12:37:00 +0700

    update constant.ts

--- frontend-mobile/src/utils/constants.ts
+++ frontend-mobile/src/utils/constants.ts
@@ -1,8 +1,8 @@
 import { Dimensions } from "react-native";
 
-export const BASE_API_URL = "http://192.168.43.146:4000";
+export const BASE_API_URL = "http://192.168.1.9:4000";
 export const PROD_BASE_API_URL = "https://just-eats-api.herokuapp.com/graphql";
-export const WS_BASE_API_URL = "ws://192.168.43.146:4000";
+export const WS_BASE_API_URL = "ws://192.168.1.9:4000";
 export const PROD_WS_BASE_API_URL = "ws://just-eats-api.herokuapp.com/graphql";
 export const TOKEN_KEY = "TOKEN_KEY";
 
